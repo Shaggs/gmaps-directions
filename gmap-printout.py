d71e5766e49b7907cc069659227bf0df4f0fb46f
Shaggy <rees.shane@gmail.com>
2015-09-12 21:00:51 +0930

    Change of name, Print out for windows only

--- gmap-printout.py
+++ gmap-printout.py
@@ -1,8 +1,13 @@
 import json, urllib, re
 from urllib import urlencode
 import googlemaps
-start = "" #add address, State, Country as shown
-finish = "" #add address, State, Country as shown
+import tempfile
+import win32api
+import win32print
+start = raw_input("Start Address: ")
+finish = raw_input("finish address: ")
+
+
 
 url = 'http://maps.googleapis.com/maps/api/directions/json?%s' % urlencode((
 			('origin', start),
@@ -10,11 +15,20 @@ url = 'http://maps.googleapis.com/maps/api/directions/json?%s' % urlencode((
  ))
 ur = urllib.urlopen(url)
 result = json.load(ur)
-
-with open("output.txt", "w") as output:
+filename = "output.txt"
+with open(filename, "w") as output:
     for i in range (0, len (result['routes'][0]['legs'][0]['steps'])):
         s = (result['routes'][0]['legs'][0]['steps'][i]['html_instructions'])
         d = (result['routes'][0]['legs'][0]['steps'][i]['distance']['text'])
         l = (result['routes'][0]['legs'][0]['steps'][i]['duration']['text'])
+        s = re.sub('<[A-Za-z\/][^>]*>', '', s)
         print s + d + l
-        output.writelines(s + " " + d + " " + l + '\n')
+        output.writelines(s + " " + d + "  " + l + '\n')
+win32api.ShellExecute (
+	0,
+	"print",
+	filename,
+	'/d:"%s"' % win32print.GetDefaultPrinter (),
+	'.',
+	0
+	)
